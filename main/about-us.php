7f58f91536fc6f1e96dd1694f8e1a0f26d9594a4
Khushboo-Mittal2004 <agrawalkhushboo8954@gmail.com>
2025-04-20 12:44:29 +0530

    Initial commit

---
+++ main/about-us.php
@@ -0,0 +1,119 @@
+<?php
+include("header.php");
+?>
+            
+            <!-- breadcrumb-area start -->
+            <div class="breadcrumb-area bg-gray">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <ul class="breadcrumb-list">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item active">About us</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- breadcrumb-area end -->
+            
+            <!-- content-wraper start -->
+            <div class="content-wraper mt-95">
+                <div class="container">
+                    <div class="row ">
+                        <div class="col-lg-6">
+                            <div class="about-us-img">
+                                <img alt="" src="images/online-auction.jpg">
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="about-info-wrapper">
+                                <h2>Our company</h2>
+<p>Welcome to our online property auction platform RealEstateAuctionX, where buying and selling real estate has never been easier! We are a team of experienced professionals dedicated to simplifying the real estate market and providing an efficient and transparent online platform for property auctions.</p>
+
+<p>Our platform is designed to make the auction process more accessible and convenient for both buyers and sellers. With our user-friendly interface, you can easily browse through a wide selection of properties and place bids from the comfort of your own home. Our advanced search tools allow you to filter properties by location, type, and price range, making it easy to find exactly what you're looking for.</p>
+
+<p>As a seller, you can trust us to get your property in front of a wide audience of potential buyers. We provide comprehensive marketing support to ensure maximum exposure for your property, and our auction process ensures that you receive fair market value for your property.</p>
+
+<p>At our online property auction platform, we pride ourselves on providing exceptional customer service. Our team is always on hand to answer any questions you may have, and we are committed to making your auction experience as smooth and stress-free as possible.</p>
+
+<p>Thank you for choosing our online property auction platform for your real estate needs. We look forward to helping you buy or sell your next property!
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- content-wraper end -->
+            
+            <div class="project-count-area bg-gray pt-80 pb-50 mt-95">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="single-count text-center mb-30">
+                                <div class="count-icon">
+                                    <span class="ion-ios-briefcase-outline"></span>
+                                </div>
+                                <div class="count-title">
+                                    <h2 class="count">
+<?php 
+$sql ="SELECT * FROM customer";
+$qsql = mysqli_query($con,$sql);
+echo  mysqli_num_rows($qsql); 
+?>
+									</h2>
+                                    <span>Customers</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="single-count text-center mb-30">
+                                <div class="count-icon">
+                                    <span class="ion-ios-wineglass-outline"></span>
+                                </div>
+                                <div class="count-title">
+                                    <h2 class="count"><?php 
+$sql ="SELECT * FROM product";
+$qsql = mysqli_query($con,$sql);
+echo  mysqli_num_rows($qsql); 
+?></h2>
+                                    <span>Auction Properties</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="single-count text-center mb-30">
+                                <div class="count-icon">
+                                    <span class="ion-ios-lightbulb-outline"></span>
+                                </div>
+                                <div class="count-title">
+                                    <h2 class="count"><?php 
+$sql ="SELECT * FROM bidding";
+$qsql = mysqli_query($con,$sql);
+echo  mysqli_num_rows($qsql); 
+?></h2>
+                                    <span>biddings</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="single-count text-center mb-30">
+                                <div class="count-icon">
+                                    <span class="ion-happy-outline"></span>
+                                </div>
+                                <div class="count-title">
+                                    <h2 class="count"><?php 
+$sql ="SELECT * FROM winners";
+$qsql = mysqli_query($con,$sql);
+echo  mysqli_num_rows($qsql); 
+?></h2>
+                                    <span>Winners</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+<?php
+include("footer.php");
+?>
